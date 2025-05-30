0b736a3dd180d378f593113675db6ab5dfcd21f7
Emmanuel Egomson <fegomson@gmail.com>
2025-05-30 00:42:04 -0500

    Add authentication context and hooks for user login and logout functionality

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,15 +4,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.tsx'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { AuthProvider } from './context/AuthContext.tsx'
 
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </AuthProvider>
   </StrictMode>,
 )
